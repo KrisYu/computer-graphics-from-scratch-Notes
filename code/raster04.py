6af46e24aebc527cfa3a61ddefa64a5197f1ef01
KrisYu <abcyuxue123@126.com>
2019-04-14 10:45:13 +0800

    draw two cubes

--- code/raster04.py
+++ code/raster04.py
@@ -5,60 +5,61 @@
 from PIL import Image, ImageDraw
 import math
 
-
-viewport_size = 1.0
-projection_plane_z = 1.0
-
-
-class Point(object):
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
-
-
-class Vertex(object):
-	def __init__(self, x, y, z):
-		self.x = x
-		self.y = y
-		self.z = z
-
-		
+from gameobjects.vector3 import Vector3
+from gameobjects.vector2 import Vector2
 
 
 def drawLine(p0, p1, color):
-	print p0.x, p0.y, p1.x ,p1.y
 
-	x0 = screen_width / 2 + p0.x 
-	y0 = screen_height / 2 - p0.y
+	p0 = canvasToScreen(p0)
+	p1 = canvasToScreen(p1)
 
-	x1 = screen_width / 2 + p1.x
-	y1 = screen_height / 2 - p1.y
+	x0, y0 = p0.x, p0.y
+	x1, y1 = p1.x, p1.y
 
 	draw = ImageDraw.Draw(image)
 	draw.line((x0, y0, x1, y1), fill = color)
 
 def viewportToCanvas(p2d):
-	return Point(p2d.x * screen_width / viewport_size,
+	"""
+	p2d: vector2
+	rtype: vector2
+	"""
+	return Vector2(p2d.x * screen_width / viewport_size,
 		p2d.y * screen_height / viewport_size)
 
-def projectVertex(v):
-	return viewportToCanvas(Point(v.x * projection_plane_z / v.z,
-		v.y * projection_plane_z / v.z))
+def projectVertex(v3):
+	"""
+	v3: vector
+	rtype: vector2
+	"""
+	return viewportToCanvas(Vector2(v3.x * projection_plane_z / v3.z,
+		v3.y * projection_plane_z / v3.z))
+
+def canvasToScreen(v2):
+	"""
+	v2: vector2
+	rtype: vector2 on screen
+	"""
+	return Vector2(screen_width / 2 + v2.x, screen_height / 2 - v2.y)
 
 
 screen_width = 600
 screen_height = 600
 
 
-vA = Vertex(-2, -0.5, 5)
-vB = Vertex(-2, 0.5, 5)
-vC = Vertex(-1, 0.5, 5)
-vD = Vertex(-1, -0.5, 5)
+viewport_size = 1.0
+projection_plane_z = 1.0
 
-vAb = Vertex(-2, -0.5, 6)
-vBb = Vertex(-2, 0.5, 6)
-vCb = Vertex(-1, 0.5, 6)
-vDb = Vertex(-1, -0.5, 6)
+vA = Vector3(-2, -0.5, 5)
+vB = Vector3(-2, 0.5, 5)
+vC = Vector3(-1, 0.5, 5)
+vD = Vector3(-1, -0.5, 5)
+
+vAb = Vector3(-2, -0.5, 6)
+vBb = Vector3(-2, 0.5, 6)
+vCb = Vector3(-1, 0.5, 6)
+vDb = Vector3(-1, -0.5, 6)
 
 
 RED = (255, 0, 0)
@@ -70,19 +71,19 @@ background_color = (255, 255, 255, 255)
 
 image = Image.new("RGBA", (screen_width, screen_height), background_color)
 
-drawLine(projectVertex(vA),projectVertex(vB), BLUE)
-drawLine(projectVertex(vB),projectVertex(vC), BLUE)
-drawLine(projectVertex(vC),projectVertex(vD), BLUE)
-drawLine(projectVertex(vD),projectVertex(vA), BLUE)
+drawLine(projectVertex(vA),projectVertex(vB), BLUE);
+drawLine(projectVertex(vB),projectVertex(vC), BLUE);
+drawLine(projectVertex(vC),projectVertex(vD), BLUE);
+drawLine(projectVertex(vD),projectVertex(vA), BLUE);
 
-drawLine(projectVertex(vAb),projectVertex(vBb), RED)
-drawLine(projectVertex(vBb),projectVertex(vCb), RED)
-drawLine(projectVertex(vCb),projectVertex(vDb), RED)
-drawLine(projectVertex(vDb),projectVertex(vAb), RED)
+drawLine(projectVertex(vAb),projectVertex(vBb), RED);
+drawLine(projectVertex(vBb),projectVertex(vCb), RED);
+drawLine(projectVertex(vCb),projectVertex(vDb), RED);
+drawLine(projectVertex(vDb),projectVertex(vAb), RED);
 
-drawLine(projectVertex(vA),projectVertex(vAb), GREEN)
-drawLine(projectVertex(vB),projectVertex(vBb), GREEN)
-drawLine(projectVertex(vC),projectVertex(vCb), GREEN)
-drawLine(projectVertex(vD),projectVertex(vDb), GREEN)
+drawLine(projectVertex(vA),projectVertex(vAb), GREEN);
+drawLine(projectVertex(vB),projectVertex(vBb), GREEN);
+drawLine(projectVertex(vC),projectVertex(vCb), GREEN);
+drawLine(projectVertex(vD),projectVertex(vDb), GREEN);
 
 image.save("raster04.png")
---
+++ code/raster06.py
@@ -0,0 +1,149 @@
+#-*- coding: utf-8 -*-
+#!python
+
+#raster-01.py
+from PIL import Image, ImageDraw
+import math
+
+from gameobjects.vector3 import Vector3
+from gameobjects.vector2 import Vector2
+
+
+class Triangle(object):
+	"""docstring for Triangle"""
+	def __init__(self, v0, v1, v2, color):
+		self.v0 = v0
+		self.v1 = v1
+		self.v2 = v2
+		self.color = color
+
+class Model(object):
+	def __init__(self, vertexes, triangles):
+		self.vertexes = vertexes
+		self.triangles = triangles
+
+class Instance(object):
+	def __init__(self, model, position):
+		self.model = model
+		self.position = position
+	
+
+def renderTriangle(triangle, projected):
+	p0, p1, p2, color = projected[triangle.v0], projected[triangle.v1], projected[triangle.v2], triangle.color
+	p0, p1, p2 = canvasToScreen(p0), canvasToScreen(p1), canvasToScreen(p2)
+
+	x0, y0 = p0.x, p0.y
+	x1, y1 = p1.x, p1.y
+	x2, y2 = p2.x, p2.y
+
+	draw = ImageDraw.Draw(image)
+	draw.line((x0, y0, x1, y1),fill = color)
+	draw.line((x1, y1, x2, y2),fill = color)
+	draw.line((x2, y2, x0, y0),fill = color)
+
+
+def renderObject(vertexes, triangles):
+	"""
+	vertexes: vector3
+	triangles: 
+	"""
+	projected = []
+	for vertex in vertexes:
+		projected.append(projectVertex(vertex))
+
+	for triangle in triangles:
+		renderTriangle(triangle, projected)
+
+def viewportToCanvas(p2d):
+	"""
+	p2d: vector2
+	rtype: vector2
+	"""
+	return Vector2(p2d.x * screen_width / viewport_size,
+		p2d.y * screen_height / viewport_size)
+
+def projectVertex(v3):
+	"""
+	v3: vector
+	rtype: vector2
+	"""
+	return viewportToCanvas(Vector2(v3.x * projection_plane_z / v3.z,
+		v3.y * projection_plane_z / v3.z))
+
+def canvasToScreen(v2):
+	"""
+	v2: vector2
+	rtype: vector2 on screen
+	"""
+	return Vector2(screen_width / 2 + v2.x, screen_height / 2 - v2.y)
+
+
+def renderInstance(instance):
+	"""
+	"""
+	projected = []
+	model = instance.model
+
+	for vertex in model.vertexes:
+		projected.append(projectVertex(vertex + instance.position))
+
+	for triangle in model.triangles:
+		renderTriangle(triangle, projected)
+
+
+def renderScene(instances):
+	for instance in instances:
+		renderInstance(instance)
+
+screen_width = 600
+screen_height = 600
+
+viewport_size = 1.0
+projection_plane_z = 1.0
+
+vertexes =  [
+  Vector3(1, 1, 1),
+  Vector3(-1, 1, 1),
+  Vector3(-1, -1, 1),
+  Vector3(1, -1, 1),
+  Vector3(1, 1, -1),
+  Vector3(-1, 1, -1),
+  Vector3(-1, -1, -1),
+  Vector3(1, -1, -1)
+]
+
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+PURPLE = (255, 0, 255)
+CYAN = (0, 255, 255)
+
+triangles = [
+  Triangle(0, 1, 2, RED),
+  Triangle(0, 2, 3, RED),
+  Triangle(4, 0, 3, GREEN),
+  Triangle(4, 3, 7, GREEN),
+  Triangle(5, 4, 7, BLUE),
+  Triangle(5, 7, 6, BLUE),
+  Triangle(1, 5, 6, YELLOW),
+  Triangle(1, 6, 2, YELLOW),
+  Triangle(4, 5, 1, PURPLE),
+  Triangle(4, 1, 0, PURPLE),
+  Triangle(2, 6, 7, CYAN),
+  Triangle(2, 7, 3, CYAN),
+];
+
+
+
+background_color = (255, 255, 255, 255)
+image = Image.new("RGBA", (screen_width, screen_height), background_color)
+
+cube = Model(vertexes, triangles)
+
+instances = [Instance(cube, Vector3(-1.5, 0, 7)),Instance(cube, Vector3(1.25, 2, 7.5))]
+
+renderScene(instances)
+
+image.save("raster06.png")
